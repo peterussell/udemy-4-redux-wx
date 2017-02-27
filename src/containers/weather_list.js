2edd080499c9d357becbc21621e002328c7b2001
Pete Russell <pinkbatz@gmail.com>
2017-02-26 18:52:07 -0800

    Adds framework for weather list table.

---
+++ src/containers/weather_list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// We can use some ES6 to quickly pull the weather property off state,
+// normally we would write:
+//  > mapStateToProps(state) { return { weather: state.weather }; }
+// But with ES6 we can get the weather property on state directly by
+// using curly braces inside the argument list. This is the same as
+// writing:
+//  > const weather = state.weather;
+// ... inside the function body.
+/* function mapStateToProps({ weather }}) { // weather is from state.weather;
+  return { weather: weather };
+} */
+
+// Then recall, whenever we have a key and value with the same identifier,
+// then we can condense that down by using curly braces:
+//  > { weather: weather } is the same as { weather } ...
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,4 @@
+import { FETCH_WEATHER } from '../actions/index';
 
 // Recall: reducers are just functions which take the particular
 // piece of state this Reducer's responsible for, an action, and
@@ -5,6 +6,27 @@
 
 // We make sure this Reducer gets called by adding it to
 // reducers/index.js
-export default function(state = null, action) {
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // Recall: we should never modify existing state, we should only
+      // create new state and return that new state. So something like
+      // state.push(action.payload.data) is BAD - we never mutate state
+      // directly.
+      // Because we want to collect each city that gets searched for,
+      // we want create a new array which contains all the old weather
+      // data, as well as the new weather data, and then assign this
+      // to the state we return.
+      // Non-ES6, we could use state.concat([action.payload.data]);
+      // In ES6, we can use the 'spread' operator:
+      return [ action.payload.data, ...state ];
+      // Results in [city,city,city] - NOT [city,[city,city]]
+
+      // Also note that we can switch whether the new data goes at the
+      // head of the array:
+      //  > [ action.payload.data, ...state ];
+      // Or the tail of the array:
+      //  > [ ..state, action.payload.data ];
+  }
   return state;
 }
